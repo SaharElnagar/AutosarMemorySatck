8f03314bec03794b09ce75bf905838d797d13606
Sahar <sa>
2020-02-17 21:59:21 +0200

    Added Ea module

---
+++ Software/bsw/gen/Ea_Cfg.h
@@ -0,0 +1,17 @@
+
+/*
+ * Eep_Cfg.h
+ *
+ *  Created on: Nov 24, 2019
+ *      Author: Sahar
+ */
+
+#ifndef EEP_CFG_H_
+#define EEP_CFG_H_
+
+#include "MemIf_Types.h"
+#include "Eep.h"
+
+
+
+#endif /* EEP_CFG_H_ */
