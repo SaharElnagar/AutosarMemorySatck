e3153393fd21881912a967491950811b472fa6f9
Yomna Mokhtar <yomnamok@gmail.com>
2020-06-03 00:43:00 +0200

    NvM update

--- Software/bsw/stub/Det.h
+++ Software/bsw/stub/Det.h
@@ -1,10 +1,20 @@
+/*******************************************************************************
+**                                                                            **
+**  FILENAME     : Det.h                                                      **
+**                                                                            **
+**  VERSION      : 4.3.1                                                      **
+**                                                                            **
+**  DATE         : 2019-12-1                                                  **
+**                                                                            **
+**  PLATFORM     : TIVA C                                                     **
+**                                                                            **
+**  AUTHOR       : Yomna Mokhtar                                              **
+**                                                                            **
+**                                                                            **
+*******************************************************************************/
 
 #ifndef __DET_H__
 #define __DET_H__
-
-
-
-
 
 #include "Std_Types.h"
 
@@ -13,10 +23,8 @@
 /*****************************************************************************************/
 
 
-
-
 Std_ReturnType Det_ReportError(uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 ErrorId);
-
+Std_ReturnType Det_ReportTransientFault(uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 ErrorId);
+Std_ReturnType Det_ReportRuntimeError(uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 ErrorId);
 
 #endif
-
